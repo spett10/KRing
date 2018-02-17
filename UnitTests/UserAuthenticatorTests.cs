00512ee3bec60bf2b9be96579ccfe5b6c6226d2a
spett10 <stivhobbykniv@gmail.com>
2018-02-17 16:55:24 +0200

    removed todo for keycheck, doesnt add value. added unit test.

--- UnitTests/UserAuthenticatorTests.cs
+++ UnitTests/UserAuthenticatorTests.cs
@@ -29,5 +29,27 @@ namespace UnitTests
             Assert.IsTrue(passwordCorrect);
             Assert.IsTrue(usernameCorrect);
         }
+
+        [TestMethod]
+        public void UserAuthentication_WrongPassword_ShouldBeDifferent()
+        {
+            var username = "Sugga pie";
+            var password = "so secret!";
+
+            var usernameSalt = CryptoHashing.GenerateSalt();
+            var usernameRaw = Encoding.ASCII.GetBytes(username);
+            var userToken = UserAuthenticator.CreateAuthenticationToken(username, usernameSalt);
+
+            var passwordSalt = CryptoHashing.GenerateSalt();
+            var passwordRaw = Encoding.ASCII.GetBytes("Some guess");
+            var passwordToken = UserAuthenticator.CreateAuthenticationToken(password, passwordSalt);
+
+            var passwordCorrect = UserAuthenticator.Authenticate(passwordRaw, passwordSalt, passwordToken);
+
+            var usernameCorrect = UserAuthenticator.Authenticate(usernameRaw, usernameSalt, userToken);
+
+            Assert.IsFalse(passwordCorrect);
+            Assert.IsTrue(usernameCorrect);
+        }
     }
 }
